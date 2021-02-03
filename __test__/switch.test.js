edc5b1847ee65b655c7fb32e38d800a1f8bb5e45
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-02 18:30:54 -0800

    Prototype: Static website to test HTML and routes before moving to backend

---
+++ __test__/switch.test.js
@@ -0,0 +1,7 @@
+describe('Test switching successful', () => {
+
+    test('This test proves that you can run two separate tests with `npm run test` and `npm run test2`. Test2 requires you to drop database between test suites.', () => {
+        expect(true).toBe(true);
+    });
+
+});
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS goal_social_db;
+
+CREATE DATABASE goal_social_db;
