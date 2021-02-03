9ad5b4154c85a0c8b2e8e3affa3c12b06040617a
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-03 06:43:15 -0800

    Added comments to guide .env errors

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,14 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// Debug:
+// Debug: Not connecting? See if you have an .env file or if the .env file is recognized
 // console.log({
 //     DB_NAME: process.env.DB_NAME,
 //     DB_USER: process.env.DB_USER,
 //     DB_PW: process.env.DB_PW,
 //     DB_PORT: process.env.DB_PORT
 // });
+// process.exit(0);
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL ?
