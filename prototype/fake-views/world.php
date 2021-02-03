0c9ae0b08e0a9a0e8d9180938f769d88f3477886
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-03 07:09:17 -0800

    Prototype: Refactored world template for readability

--- prototype/fake-views/world.php
+++ prototype/fake-views/world.php
@@ -1,5 +1,7 @@
 <span class="page-title-dynamic d-none">{{pageTitle}}</span> <!-- Remove line when refactored into production code -->
+
 <div class="mb-3">World View. All Public posts, goals, milestones, and comments are here. To show a sample to attract signups.</div>
+
 {{#each posts}}
 <div class="bordered mb-2">
     <div class="bordered-alt">Post id: {{_id}}</div>
