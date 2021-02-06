1788fea3dd2a3ae74e6f699c5b0485baf3a77840
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-06 03:53:59 -0800

    Prototype: Goal Planner has milestone inputs

--- prototype/fake-views/goal-planner.php
+++ prototype/fake-views/goal-planner.php
@@ -1,14 +1,43 @@
 <style>
+/** Placeholder on empty */
 .goal-planner-page [contenteditable]:empty:before {
     content: attr(data-placeholder);
 }
 .goal-planner-page [contenteditable]:empty {
     opacity: .5;
 }
+/* Date visual */
 .date {
   margin-left: -20px;
   width: 100%;
   text-align: center;
+}
+
+/* Milestone inputs and buttons */
+.milestone {
+  display: flex;
+}
+.milestone, .milestone-detail {
+  margin-bottom: 5px;
+  height: 30px;
+}
+.milestone input {
+  width: 100%;
+}
+.milestone button {
+  width: 40px;
+  height: 30px;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+.milestone-add {
+  display: block;
+  margin-top: 10px;
+}
+
+/* Milestone details */
+.milestone-detail {
+  width: 90%;
 }
 </style>
 
@@ -26,12 +55,44 @@
         <div class="item item4">
           <p><span><i class="fa fa-star"></i> Milestones:</span></p>
           <p class="help">(what you need to do)</p>
-          <p class="text" contenteditable spellcheck="false" data-placeholder="You can edit this! Enter specific subgoals"></p>
+          <!-- <p class="text" data-placeholder="You can edit this! Enter specific subgoals"> -->
+          <div class="milestones">
+
+            <div class="milestone">
+              <input class="milestone" type="text"/>
+              <button class="milestone-delete btn btn-danger float-end" onclick="deleteMilestone($(event.target))"><i class="fa fa-times"></i></button>
+            </div>
+          </div>
+          <button class="milestone-add btn btn-primary"><i class="fa fa-plus"></i> Add new milestone! </button>
+          </p>
+          <script>
+          function deleteMilestone($here) {
+            var $milestone = $here.closest(".milestone");
+            var whichIndex = $milestone.index();
+            $milestone.remove();
+            $(".milestone-detail").eq(whichIndex).remove();
+          }
+          $(".milestone-add").on("click", ()=>{
+            $(".milestones").append(`
+            <div class="milestone">
+              <input class="milestone" type="text"/>
+              <button class="milestone-delete btn btn-danger float-end" onclick="deleteMilestone($(event.target))"><i class="fa fa-times"></i></button>
+            </div>
+            `);
+
+            $(".milestone-details").append(`
+              <input class="milestone-detail" type="text"/>
+            `)
+          })
+          </script>
         </div>
         <div class="item item3">
           <p><span>Strategy:</span></p>
           <p class="help">(what you need to do)</p>
-          <p class="text" contenteditable spellcheck="false" data-placeholder="You can edit this! Enter more details"></p>
+          <div class="milestone-details">
+            <input class="milestone-detail" type="text"/>
+            <!-- <p class="text milestone-detail" contenteditable spellcheck="false" data-placeholder="You can edit this! Enter more details"></p> -->
+          </div>
         </div>
       </div>
       <div class="grid3">
@@ -41,7 +102,7 @@
           <p class="text" contenteditable spellcheck="false" data-placeholder="You can edit this! Enter reasons why you want to achieve this goal"></p>
         </div>
         <div class="grid4">
-          <div class="item item6">
+          <div class="item item6 pt-3">
             <p class="date"><span><i class="fa fa-star"></i> Start Date</span></p>
             <p><input type="date" class="date-start" /></p>
           </div>
