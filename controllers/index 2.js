a14138094da636010e26a3549fa4ecb31a6778b5
Aidan <70040873+XXLZopes@users.noreply.github.com>
2021-02-08 20:20:09 -0800

    Delete index 2.js

--- controllers/index 2.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const htmlRoutes = require('./html-routes.js');
-const apiRoutes = require('./api/');
-
-router.use('/', htmlRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
